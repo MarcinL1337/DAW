b5beefb66574ae047d68232cf0f4048e06a6ee54
MarcinL1337 <marcin.linkiewicz1337@gmail.com>
2025-04-07 22:49:18 +0200

    temp

--- Source/SideMenu/SideMenu.cpp
+++ Source/SideMenu/SideMenu.cpp
@@ -1,9 +1,29 @@
 #include "SideMenu.h"
 
+SideMenu::SideMenu() { initSliders(); }
+
 void SideMenu::paint(juce::Graphics& g)
 {
-    g.setColour(juce::Colours::lightgrey);
+    g.setColour(juce::Colours::whitesmoke);
     g.drawRect(getLocalBounds());
 }
 
-void SideMenu::resized() {}
+void SideMenu::resized() { positionSliders(); }
+
+void SideMenu::initSliders()
+{
+    addAndMakeVisible(faderSlider);
+    faderSlider.setRange(-15, 15);
+    faderSlider.setTextValueSuffix(" dB");
+    faderSlider.
+
+        addAndMakeVisible(faderLabel);
+    faderLabel.setText("Fader", juce::dontSendNotification);
+    faderLabel.attachToComponent(&faderSlider, false);
+}
+
+void SideMenu::positionSliders()
+{
+    faderSlider.setSize(getWidth() * 0.95, sliderHeight);
+    faderSlider.setCentrePosition(getWidth() * 0.5, 50);
+}
--- Source/SideMenu/SideMenu.h
+++ Source/SideMenu/SideMenu.h
@@ -5,9 +5,25 @@
 class SideMenu final : public juce::Component
 {
 public:
-    SideMenu() = default;
+    SideMenu();
     ~SideMenu() override = default;
 
     void paint(juce::Graphics& g) override;
     void resized() override;
+
+    void positionSliders();
+    void initSliders();
+
+private:
+    juce::Slider faderSlider;
+    juce::Label faderLabel;
+    juce::Slider delaySlider;
+    juce::Label delayLabel;
+    juce::Slider reverbSlider;
+    juce::Label reverbLabel;
+    juce::Slider bassBoostSlider;
+    juce::Label bassBoostLabel;
+
+    const uint8_t sliderPadding{20u};
+    const uint8_t sliderHeight{40u};
 };
